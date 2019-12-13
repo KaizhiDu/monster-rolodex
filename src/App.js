5468ac7c2c2056e2fb995f362e1bb984ccb5c702
kaizhi du <kaizhidu@kaizhis-MacBook-Pro.local>
2019-12-13 13:07:13 -0800

    show fetch user name

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import './App.css';
+import { CardList } from './cpmponents/card-list/card-list.component';
 
 class App extends Component {
 
@@ -7,7 +8,7 @@ class App extends Component {
         super();
         this.state = {
             monsters: []
-        }
+        };
     }
 
     componentDidMount() {
@@ -19,13 +20,9 @@ class App extends Component {
     render() {
         return (
             <div className="App">
-                {this.state.monsters.map(monster => (
-                    <h1 key={monster.id}>
-                        {monster.name}
-                    </h1>
-                ))}
+                <CardList monsters={this.state.monsters}/>
             </div>
-        )
+        );
     }
 
 }
